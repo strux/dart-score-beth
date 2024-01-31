1545b9ef420ee2275c9799c4efbe01b59e556d8c
Ben Carney <ben@carnivorous.org>
2024-01-31 10:14:53 -0800

    Basic styles and functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { Elysia } from "elysia";
 import { html } from "@elysiajs/html";
 import { staticPlugin } from "@elysiajs/static";
+import clsx from "clsx";
+
+const game = {
+  targets: ["20", "19"],
+};
 
 let players = [
-  { name: "test1", score: { "20": 0, "19": 0 } },
-  { name: "test2", score: { "20": 0, "19": 0 } },
+  { name: "Brian", score: { "20": 0, "19": 0 } },
+  { name: "Danielle", score: { "20": 0, "19": 0 } },
 ];
 
 function Page({ children }: any) {
@@ -14,10 +19,16 @@ function Page({ children }: any) {
         <title>Dart Score</title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kolker+Brush&display=swap"
+          rel="stylesheet"
+        ></link>
         <link href="./public/output.css" rel="stylesheet" />
         <script src="./public/htmx.min.js"></script>
       </head>
-      <body class="bg-slate-900 text-slate-300 flex-col align-middle">
+      <body class="bg-slate-800 text-slate-300 font-sans flex flex-col justify-center items-center text-center font-script text-6xl">
         {children}
       </body>
     </html>
@@ -25,37 +36,80 @@ function Page({ children }: any) {
 }
 
 function Board({ players }) {
+  const colCount = players.length * 2 - 1;
+  const content = [];
+  for (let i = 0; i < colCount; i++) {
+    if (i % 2 === 0) {
+      const player = players[i / 2];
+      content[i] = <Score player={player} />;
+    } else {
+      content[i] = <Labels targets={game.targets} />;
+    }
+  }
   return (
-    <div class="flex justify-center">
-      <Winner />
-      {players.map((player) => (
-        <div class="flex-col items-center justify-center text-center m-2">
-          <div>{player.name}</div>
-          {Object.keys(player.score).map((key) => (
-            <Score player={player} target={key} />
-          ))}
+    <div class="border p-[6px]">
+      <div class="border-[4px] p-[6px]">
+        <div class="border p-6">
+          <Winner />
+          <div class="flex">{content}</div>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function Score({ player }) {
+  return (
+    <div class="player-score">
+      <div class="border-b-2 w-40 flex items-center justify-center text-center ">
+        {player.name}
+      </div>
+      {Object.keys(player.score).map((target) => (
+        <button
+          class="block w-full text-8xl leading-[4rem]"
+          hx-put={`./player-score/${player.name}?target=${target}`}
+          hx-swap="outerHTML"
+          hx-target="closest .player-score"
+        >
+          {player.score[target]}
+        </button>
       ))}
     </div>
   );
 }
 
-function Score({ player, target }) {
+function Labels({ targets }) {
   return (
-    <form class="score" hx-put="./score" hx-sync="this:queue first">
-      <input type="hidden" name="target" value={target} />
-      <input type="hidden" name="player" value={player.name} />
-      <button type="submit">{player.score[target]}</button>
-    </form>
+    <div class="">
+      <div class="border-b-2">vs</div>
+      {targets.map((target) => (
+        <div class="block leading-[4rem] border-x-2 px-4">{target}</div>
+      ))}
+    </div>
   );
 }
 
 function Winner({ name }) {
   return (
-    <h1 hx-get="./winner" hx-trigger="score-updated from:body once" hx-sync="">
+    <h1
+      hx-get="./winner"
+      hx-trigger="score-updated from:body once"
+      hx-swap="outerHTML"
+      class="min-h-8"
+    >
       {name}
     </h1>
   );
+}
+
+function getPlayerByName(name) {
+  return players.find((p) => p.name === name);
+}
+
+function updatePlayerScore(playerName, target) {
+  const player = getPlayerByName(playerName);
+  if (player?.score[target] < 3) player.score[target] += 1;
+  return player;
 }
 
 const app = new Elysia()
@@ -68,14 +122,13 @@ const app = new Elysia()
       </Page>
     );
   })
-  .put("/score", ({ set, body }) => {
-    const player = players.find((p) => p.name === body.player);
-    const target = body.target;
-    if (player?.score[target] < 3) player.score[target] += 1;
-
-    set.headers["HX-Trigger"] = "score-updated";
-    return <Score player={player} target={target} />;
-  })
+  .put(
+    "/player-score/:playerName",
+    ({ set, params: { playerName }, query: { target } }) => {
+      set.headers["HX-Trigger"] = "score-updated";
+      return <Score player={updatePlayerScore(playerName, target)} />;
+    }
+  )
   .get("/winner", () => {
     const winner =
       players.find((p) => Object.values(p.score).every((s) => s >= 3))?.name ||
@@ -83,6 +136,11 @@ const app = new Elysia()
     return <Winner name={winner} />;
   })
   .listen(3000);
+
+process.on("SIGINT", () => {
+  console.log("Ctrl-C was pressed");
+  process.exit();
+});
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
+  safelist: [
+    {
+      pattern: /grid-rows-.+/,
+    }
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+         'script': ['Kolker Brush'],
+      }
+   }
   },
   plugins: [],
 }
-
